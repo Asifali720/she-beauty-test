aca84304fea0384940a313d620607f28972b8db3
Asifali720 <asiflashari720@gmail.com>
2025-01-17 06:51:45 +0500

    fix

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -3,6 +3,8 @@ const { join } = require('path')
 /**
  * @type {import("puppeteer").Configuration}
  */
+
+console.log('Puppeteer cache directory:', join(__dirname, '.cache', 'puppeteer'))
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer')
--- src/helpers/pdf-distributor-ledger.ts
+++ src/helpers/pdf-distributor-ledger.ts
@@ -4,7 +4,11 @@ import { formatTime } from '@/@core/utils/format'
 
 export async function createDistributorPdf({ distributor, mergedData, startDate, endDate }: any) {
   // eslint-disable-next-line import/no-named-as-default-member
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+    executablePath: '/opt/build/cache/puppeteer/chrome-linux/chrome'
+  })
   const page = await browser.newPage()
 
   const SheBeautyLogo =
